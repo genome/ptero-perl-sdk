7a47c04438353589a529a72bf451cb5b1e031b10
David Morton <davidlmorton@gmail.com>
2014-10-01 17:19:26 -0500

    Overhaul Method

--- lib/Ptero/WorkflowBuilder/Detail/Method.pm
+++ lib/Ptero/WorkflowBuilder/Detail/Method.pm
@@ -4,6 +4,10 @@ use Moose;
 use MooseX::Aliases;
 use warnings FATAL => 'all';
 
+use Data::Dump qw();
+use Params::Validate qw(validate validate_pos :types);
+use Set::Scalar;
+
 with 'Ptero::WorkflowBuilder::Detail::ConvertsToHashref';
 
 has name => (
@@ -12,10 +16,9 @@ has name => (
     required => 1,
 );
 
-has submit_url => (
+has service => (
     is => 'rw',
     isa => 'Str',
-    alias => 'submitUrl',
     required => 1,
 );
 
@@ -25,17 +28,124 @@ has parameters => (
     required => 1,
 );
 
+has constant_parameters => (
+    is => 'rw',
+    isa => 'HashRef',
+    alias => 'constantParameters',
+    predicate => 'has_constant_parameters',
+);
+
+has parent_method => (
+    is => 'rw',
+    isa => 'Ptero::WorkflowBuilder::Detail::Method',
+    predicate => 'has_parent_method',
+    clearer => 'clear_parent_method',
+);
+
+has _parameter_calculation_method => (
+    is => 'rw',
+    isa => 'Ptero::WorkflowBuilder::Detail::Method',
+    predicate => 'has_parameter_calculation_method',
+    clearer => 'clear_parameter_calculation_method',
+);
+
+sub set_parameter_calculation_method {
+    my ($self, $method) = validate_pos(@_, 1, 1);
+
+    if ($method->has_parent_method) {
+        die sprintf("Method (%s) is already calculating properties for method (%s)",
+            $method->name, $method->parent_method->name);
+    }
+
+    if ($self->lineage->member($method)) {
+        die sprintf("Method (%s) is a part of the lineage containing: %s",
+            $method->name, Data::Dump::pp(map {$_->name} $self->lineage->members));
+    }
+
+    if ($self->has_parameter_calculation_method) {
+        my $old_method = $self->_parameter_calculation_method;
+        $old_method->clear_parent_method;
+    }
+
+    $method->parent_method($self);
+    $self->_parameter_calculation_method($method);
+    return $method;
+}
+
+sub lineage {
+    my $self = shift;
+
+    my $result = Set::Scalar->new($self);
+
+    my $target = $self;
+    while ($target->has_parent_method) {
+        my $parent = $target->parent_method;
+        $result->insert($parent);
+        $target = $parent;
+    }
+    return $result;
+}
+
+sub unset_parameter_calculation_method {
+    my $self = shift;
+
+    my $old_method = $self->_parameter_calculation_method;
+    $old_method->clear_parent_method;
+    $self->clear_parameter_calculation_method;
+
+    return $old_method;
+}
 
 sub to_hashref {
     my $self = shift;
 
-    return {
+    my $result ={
         name       => $self->name,
-        submitUrl  => $self->submit_url,
+        service => $self->service,
         parameters => $self->parameters,
     };
+    if ($self->has_parameter_calculation_method) {
+        $result->{'parameterCalculationMethod'} =
+            $self->_parameter_calculation_method->to_hashref;
+    }
+    if ($self->has_constant_parameters) {
+        $result->{'constantParameters'} = $self->constant_parameters;
+    }
+    return $result;
+}
+
+sub from_hashref {
+    my ($class, $hashref) = @_;
+
+    my %working_hashref = %$hashref;
+
+    my $child_hashref = delete $working_hashref{parameterCalculationMethod};
+    my $self = Ptero::WorkflowBuilder::Detail::Method->new(%working_hashref);
+    if ($child_hashref) {
+        $self->set_parameter_calculation_method(
+            Ptero::WorkflowBuilder::Detail::Method->from_hashref($child_hashref)
+        );
+    }
+    return $self;
+}
+
+sub validation_errors {
+    my $self = shift;
+    my @errors;
+
+    my $parameters = Set::Scalar->new(keys %{$self->parameters});
+    my $constants = Set::Scalar->new(keys %{$self->constant_parameters});
+
+    my $intersection = $parameters->intersection($constants);
+    if ($intersection) {
+        push @errors, sprintf(
+            'Method (%s) has a key collision %s between parameters and constant_parameters',
+            $self->name, Data::Dump::pp($intersection->members),
+        );
+    }
+
+    return @errors;
 }
 
 
 __PACKAGE__->meta->make_immutable;
-
--- t/WorkflowBuilder/Detail/Method.t
+++ t/WorkflowBuilder/Detail/Method.t
@@ -4,13 +4,12 @@ use warnings FATAL => 'all';
 use Test::Exception;
 use Test::More;
 
-
 use_ok('Ptero::WorkflowBuilder::Detail::Method');
 
 {
     my $hashref = {
         name => 'foo',
-        submitUrl => 'http://example.com',
+        service => 'bar',
         parameters => {
             hashthing => {
                 keya => 'meow',
@@ -24,10 +23,74 @@ use_ok('Ptero::WorkflowBuilder::Detail::Method');
     };
 
     my $method = Ptero::WorkflowBuilder::Detail::Method->from_hashref($hashref);
+    is_deeply($method->to_hashref, $hashref, 'round trip hashref');
 
-    is_deeply($method->to_hashref, $hashref, 'round trip hashref to method');
+    $hashref->{constantParameters} = {
+        another_hashthing => {
+            keya => 'meow',
+            keyb => 'woof',
+        },
+        another_arraything => [
+            'one', 2, 'octopus',
+        ],
+        another_scalarthing => 'corgis everywhere',
+    };
+
+    $method = Ptero::WorkflowBuilder::Detail::Method->from_hashref($hashref);
+    is_deeply($method->to_hashref, $hashref, 'round trip hashref with constantParameters');
 };
 
+{
+    my $method = Ptero::WorkflowBuilder::Detail::Method->new(
+        name => 'foo', service => 'bar',
+        parameters => { a => 'b', c => 'd'},
+        constant_parameters => { a => 'b', e =>'f' },
+    );
+    is_deeply([$method->validation_errors],
+        ['Method (foo) has a key collision "a" between parameters and constant_parameters'],
+        'key collision results in error');
+};
+
+subtest 'parameter_calculation_method' => sub{
+    my $parent = create_method('parent');
+    my $child = create_method('child');
+    $parent->set_parameter_calculation_method($child);
+
+    is($child->parent_method, $parent, 'parent relationship gets set up');
+    is_deeply($child->lineage, Set::Scalar->new($child, $parent), 'lineage is set up');
+    throws_ok sub {$child->set_parameter_calculation_method($parent)},
+        qr/lineage/, 'cycle detection works';
+    throws_ok sub {$parent->set_parameter_calculation_method($child)},
+        qr/already calculating/, 'cannot add twice';
+
+    my $expected_hashref = {
+        %{method_hashref('parent')},
+        parameterCalculationMethod => {
+            %{method_hashref('child')},
+        },
+    };
+    is_deeply($parent->to_hashref, $expected_hashref, 'to_hashref');
+
+    my $from_hashref = Ptero::WorkflowBuilder::Detail::Method->from_hashref($expected_hashref);
+    is_deeply($from_hashref->to_hashref, $expected_hashref, 'roundtrip');
+
+    $parent->unset_parameter_calculation_method();
+    delete $expected_hashref->{parameterCalculationMethod};
+    is_deeply($parent->to_hashref, $expected_hashref, 'can remove');
+};
+
+sub method_hashref {
+    my $name = shift;
+    return {
+        name => $name, service => 'bar', parameters => { a => 'b', },
+    };
+}
+
+sub create_method {
+    my $name = shift;
+
+    return Ptero::WorkflowBuilder::Detail::Method->new(method_hashref($name));
+}
 
 done_testing();
 
