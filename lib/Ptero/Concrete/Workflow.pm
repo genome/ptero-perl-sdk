d5f9e1c485297f17557e7306925b2f4014857936
David Morton <davidlmorton@gmail.com>
2015-03-04 07:00:29 +0000

    Add deterministic topological sort to Concrete::Workflow
    This might be better placed on the base-class, it is not needed
    there though.

--- lib/Ptero/Concrete/Workflow.pm
+++ lib/Ptero/Concrete/Workflow.pm
@@ -5,6 +5,7 @@ use warnings FATAL => 'all';
 
 use Params::Validate qw(validate_pos :types);
 use JSON qw();
+use Graph::Directed qw();
 
 use Ptero::Concrete::Detail::Workflow::Execution;
 use Ptero::Concrete::Detail::Workflow::Task;
@@ -17,6 +18,36 @@ has 'executions' => (
     is => 'rw',
     isa => 'HashRef[Ptero::Concrete::Detail::Workflow::Execution]',
 );
+
+# This implements a simple, but deterministic topological sorting of the tasks,
+# excluding the 'input connector' and the 'output connector'.
+sub sorted_tasks {
+    my $self = shift;
+
+    my $g = Graph::Directed->new();
+    for my $link (@{$self->links}) {
+        $g->add_edge($link->source, $link->destination);
+    }
+
+    my @result;
+    my @task_names = sort $g->successors('input connector');
+    $g->delete_vertex('input connector');
+    while (scalar($g->vertices) > 1) {
+        my $count = 0;
+        for my $name (@task_names) {
+            if ($g->in_degree($name) == 0) {
+                push @result, $name;
+                splice(@task_names, $count, 1);
+                push @task_names, sort $g->successors($name);
+                $g->delete_vertex($name);
+                last;
+            }
+            $count++;
+        }
+    }
+
+    return @result;
+}
 
 sub task_class { 'Ptero::Concrete::Detail::Workflow::Task' }
 
