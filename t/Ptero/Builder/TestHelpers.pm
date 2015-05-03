7af8b2c5f58846d3146a487861206669c7208c76
David Morton <davidlmorton@gmail.com>
2015-05-03 06:45:30 +0000

    Add fail_test to TestHelpers

--- t/Ptero/Builder/TestHelpers.pm
+++ t/Ptero/Builder/TestHelpers.pm
@@ -14,6 +14,7 @@ our @EXPORT_OK = qw(
 );
 
 sub echo_test { shift }
+sub fail_test { die "on purpose"; }
 sub sleep_echo_test { sleep(2); return shift }
 sub sleep_fail_test { sleep(2); die "Bad news"; }
 
